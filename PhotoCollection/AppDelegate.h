e87f55b42dd720a013bc377ec08c966b993b0cd3
linwei365 <linwei365@gmail.com>
2015-10-07 14:01:34 -0700

    Initial Commit

---
+++ PhotoCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoCollection
+//
+//  Created by Lin Wei on 10/7/15.
+//  Copyright © 2015 Lin Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoCollection
+//
+//  Created by Lin Wei on 10/7/15.
+//  Copyright © 2015 Lin Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
